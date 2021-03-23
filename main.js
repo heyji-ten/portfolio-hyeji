810b7a37eb217e920215a1e4803bfcf5d27e6a80
heyji-ten <59608643+heyji-ten@users.noreply.github.com>
2021-03-23 20:08:08 +0900

    JS:Navbar, Home Transparency

--- main.js
+++ main.js
@@ -12,3 +12,101 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
     }
 });
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', event => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+    return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+});
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+//Move when Navbar is selected
+const sectionIds = [
+    '#home',
+    '#about',
+    '#portfolio',
+    '#faq',
+    '#contact',
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id =>
+    document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        // 스크롤링이 아래로 되어서 페이지가 올라옴
+        if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+        } else {
+        selectedNavIndex = index - 1;
+        }
+    }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if(window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if (window.scrollY + window.innerHeight === document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
+
+// Click on the question to make the answer come out
+
+// const toggleFaq = document.querySelector('.faq__list__item');
+// toggleFaq.addEventListener('click',() => {
+//     toggleFaq.classList.toggle('on');
+// });
+
+var toggleClick = document.querySelectorAll('.faq__list__item'); 
+for (var i = 0; i < toggleClick.length; i++){
+        toggleClick[i].classList.toggle('on'); 
+}
